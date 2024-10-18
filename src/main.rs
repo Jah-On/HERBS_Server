5060cd0b3776fe16172d61eb5f0615e616213ef9
John Schulz <john.schulz1@protonmail.com>
2024-10-17 23:56:40 -0400

    Finishing HTTP server code for now.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,107 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{de::value, Deserialize, Serialize};
-use serde_json::Result;
+use serde::{Deserialize, Serialize};
 use chrono::prelude::*;
-use uuid::Uuid;
+use std::sync::Mutex;
+
+struct AppData {
+	db:      mongodb::Database,
+	nodes:   Vec<String>
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct MonitorData {
-    temperature: i8,
-    humidity:    u8,
-    preassure:   u16,
+    temperature: i8,  // Celcius
+    humidity:    u8,  // % Relative Humidity
+    pressure:    u16, // Millibars
+    accoustics:  u16, // ADC Ouput Value
+    hive_mass:   u16  // Grams
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct DbMonitorData {
+	timestamp:   String, // UTC String
+    temperature: i8,     // Celcius
+    humidity:    u8,     // % Relative Humidity
+    pressure:    u16,    // Millibars
+    accoustics:  u16,    // ADC Ouput Value
+    hive_mass:   u16     // Grams
+}
+
+//
 #[post("/{monitor_id}")]
-async fn index(path: web::Path<(String)>, req_body: String) -> impl Responder {
-    println!("Timestamp: {}", Local::now());
-    println!("{}", path);
-    let data: MonitorData;
-    println!("{}", req_body);
+async fn index(path: web::Path<String>, req_body: String, shared: web::Data<Mutex<AppData>>) -> impl Responder {
+	let locked = match shared.lock() {
+		Ok(res) => res,
+		Err(_) => return HttpResponse::Unauthorized()
+	};
+
+	match locked.nodes.contains(&path) {
+		true => {},
+		_    => return HttpResponse::NotFound()
+	}
+
+	let col: mongodb::Collection<DbMonitorData> = locked.db.collection(path.as_str());
+
+	let data: MonitorData;
     match serde_json::from_str(&req_body) {
         Ok(val) => data = val,
         Err(_)  => return HttpResponse::BadRequest()
     }
-    println!("{:?}", data);
+
+    col.insert_one(DbMonitorData{
+    	timestamp:   Utc::now().to_string(),
+     	temperature: data.temperature,
+      	humidity:    data.humidity,
+       	pressure:    data.pressure,
+        accoustics:  data.accoustics,
+        hive_mass:   data.hive_mass
+    }).await.expect("Could not insert into db!");
+
     HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+	let nodes_file = std::fs::read_to_string("nodes.txt").expect("Could not open nodes file!");
+	let mut nodes: Vec<String> = Vec::new();
+
+	for line in nodes_file.lines() {
+		nodes.push(line.to_string());
+	}
+
+	let conn: mongodb::Client = mongodb::Client::with_uri_str(
+		std::env::var("MONGODB_URI").expect(
+			"Connection string environment variable does not exist!"
+		)
+	).await.expect("Could not exstablish connection!");
+
+	let db =       conn.database("beehivesensors");
+	let existing = db.list_collection_names().await.unwrap();
+
+	for node in &nodes {
+		match existing.contains(&node) {
+			true => continue,
+			_    => db.create_collection(node).await.unwrap(),
+		}
+	}
+
+	let app_data = web::Data::new(Mutex::new(AppData{
+		db:    db,
+		nodes: nodes
+	}));
+
+	println!("*** Starting HTTP Server ***");
+
+	HttpServer::new(move || {
         App::new()
-            .service(index)
+        	.service(index)
+            .app_data(app_data.clone())
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind((std::env::var("HOST").expect(
+			"HOST environment variable does not exist!"
+		),
+    	8080
+    ))?
     .run()
     .await
 }
